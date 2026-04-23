0295261601c998ace0d66be2edcfa4ae328cef38
1-Soul-1 <ruslanpupkin789@gmail.com>
2026-04-23 09:57:22 +0500

    tests

--- config/Birds/urls.py
+++ config/Birds/urls.py
@@ -4,13 +4,13 @@ from .views import BirdsSerializer, LocationSerializer, BirdSpeciesSerializer, N
 
 
 router = DefaultRouter()
-router.register(r'birds',BirdsSerializer)
-router.register(r'location',LocationSerializer)
-router.register(r'species',BirdSpeciesSerializer)
-router.register(r'nesting_site',NestingSiteSerializer)
-router.register(r'nesting_status',NestingStatusSerializer)
-router.register(r'migration_route',MigrationRouteSerializer)
-router.register(r'during_migration',SightingDuringMigrationSerializer)
+router.register(r'Birds',BirdsSerializer)
+# router.register(r'location',LocationSerializer)
+# router.register(r'species',BirdSpeciesSerializer)
+# router.register(r'nesting_site',NestingSiteSerializer)
+# router.register(r'nesting_status',NestingStatusSerializer)
+# router.register(r'migration_route',MigrationRouteSerializer)
+# router.register(r'during_migration',SightingDuringMigrationSerializer)
 
 urlpatterns = [
     path('',include(router.urls)),
