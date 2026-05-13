acf409e7242be305e8fe486490a0711f7455052e
1-Soul-1 <ruslanpupkin789@gmail.com>
2026-05-13 12:03:21 +0500

    changes-and-birds-photo

--- config/wiki/admin.py
+++ config/wiki/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.db import models
-from wiki.models import Wiki, BirdPhoto, BirdCall  # Убрал BirdSpecies
+from wiki.models import Wiki, BirdPhoto, BirdCall
 
 @admin.register(Wiki)
 class WikiAdmin(admin.ModelAdmin):
@@ -10,10 +10,17 @@ class WikiAdmin(admin.ModelAdmin):
 
 @admin.register(BirdPhoto)
 class BirdPhotoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'bird_photo', 'author')
-    search_fields = ('name', 'author')
-    list_filter = ('bird_photo',)
+    list_display = ('id', 'name', 'bird_species', 'photographer', 'image_preview')
+    search_fields = ('name', 'photographer', 'bird_species__name')
+    list_filter = ('bird_species',)
     list_display_links = ('id', 'name')
+    
+    def image_preview(self, obj):
+        if obj.image:
+            return f'<img src="{obj.image.url}" width="100" height="auto" />'
+        return "Нет изображения"
+    image_preview.allow_tags = True
+    image_preview.short_description = "Превью"
     
     formfield_overrides = {
         models.TextField: {'widget': admin.widgets.AdminTextareaWidget(attrs={'rows': 3, 'cols': 70})},
--- config/wiki/models.py
+++ config/wiki/models.py
@@ -9,15 +9,18 @@ class Wiki(models.Model):
         return self.name
 
 class BirdPhoto(models.Model):
-    name = models.CharField(max_length=100)
-    bird_photo = models.ForeignKey(Wiki, on_delete=models.CASCADE, verbose_name="Фото птицы")
-    author = models.TextField(blank=True, null=True)
+    name = models.CharField(max_length=100, verbose_name="Название фото")
+    bird_species = models.ForeignKey('Birds.BirdSpecies', on_delete=models.CASCADE, verbose_name="Вид птицы", null=True, blank=True)
+    wiki = models.ForeignKey(Wiki, on_delete=models.CASCADE, verbose_name="Связанная статья", null=True, blank=True)
+    photographer = models.CharField(max_length=100, blank=True, null=True, verbose_name="Фотограф")
+    description = models.TextField(blank=True, null=True, verbose_name="Описание фото")
+    image = models.ImageField(upload_to='bird_photos/', blank=True, null=True, verbose_name="Изображение")  # <-- НОВОЕ ПОЛЕ
 
     def __str__(self):
         return self.name
 
 class BirdCall(models.Model):
-    name = models.CharField(max_length=100, verbose_name="Название звука/крика", default="", blank=True)  # <-- добавил default="", blank=True
+    name = models.CharField(max_length=100, verbose_name="Название звука/крика", default="", blank=True)
     bird_species = models.ForeignKey('Birds.BirdSpecies', on_delete=models.CASCADE, verbose_name="Вид птицы", null=True, blank=True)
     description = models.TextField(blank=True, null=True, verbose_name="Описание звука")
 
