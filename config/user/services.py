6c3e870412c16f40b346751e87cb1a22e19c2730
1-Soul-1 <ruslanpupkin789@gmail.com>
2026-05-19 09:28:39 +0500

    services

--- config/user/services.py
+++ config/user/services.py
@@ -1,3 +1,49 @@
 from typing import List, Set
 from django.db.models import Prefetch
 from .models import User, ObservationEntry
+
+class ObservationService:
+    
+    @staticmethod
+    def get_user_with_observations(user_id: int) -> User:
+        user = User.objects.filter(id=user_id).prefetch_related(
+            Prefetch('observationentry_set', queryset=ObservationEntry.objects.only('id', 'name', 'bird_activity'))
+        ).first()
+        return user
+    
+    @staticmethod
+    def get_observations_by_user(user_ids: List[int]) -> List[ObservationEntry]:
+        user_set = set(user_ids)
+        observations = ObservationEntry.objects.filter(observation_entry_id__in=user_set).select_related('observation_entry')
+        return list(observations)
+    
+    @staticmethod
+    def get_users_with_notes() -> List[User]:
+        users = User.objects.filter(observationentry__notes__isnull=False).distinct().prefetch_related(
+            Prefetch('observationentry_set', queryset=ObservationEntry.objects.exclude(notes='').only('id', 'name', 'notes'))
+        )
+        return list(users)
+    
+    @staticmethod
+    def filter_observations_by_activity(activity_keyword: str) -> List[ObservationEntry]:
+        observations = ObservationEntry.objects.filter(bird_activity__icontains=activity_keyword).select_related('observation_entry')
+        return list(observations)
+    
+    @staticmethod
+    def get_all_users_with_observations() -> List[User]:
+        users = User.objects.prefetch_related(
+            Prefetch('observationentry_set', queryset=ObservationEntry.objects.only('id', 'name', 'bird_activity', 'notes'))
+        ).all()
+        return list(users)
+    
+    @staticmethod
+    def get_observation_summary(user_id: int) -> dict:
+        user = User.objects.filter(id=user_id).prefetch_related('observationentry_set').first()
+        if not user:
+            return {}
+        observations = list(user.observationentry_set.all())
+        return {
+            'user_name': user.name,
+            'total_observations': len(observations),
+            'observations': observations
+        }
--- config/wiki/services.py
+++ config/wiki/services.py
@@ -1,3 +1,48 @@
 from typing import List, Set
 from django.db.models import Prefetch
 from .models import Wiki, BirdPhoto, BirdCall
+
+class WikiMediaService:
+    
+    @staticmethod
+    def get_wiki_with_photos(wiki_ids: List[int]) -> List['Wiki']:
+        wiki_set = set(wiki_ids)
+        wikis = Wiki.objects.filter(id__in=wiki_set).prefetch_related(
+            Prefetch('birdphoto_set', queryset=BirdPhoto.objects.only('id', 'name', 'photographer'))
+        )
+        return list(wikis)
+    
+    @staticmethod
+    def get_photos_by_bird_species(species_id: int) -> List['BirdPhoto']:
+        photos = BirdPhoto.objects.filter(bird_species_id=species_id).prefetch_related(
+            Prefetch('wiki', queryset=Wiki.objects.only('id', 'name'))
+        )
+        return list(photos)
+    
+    @staticmethod
+    def get_calls_by_bird_species(species_id: int) -> List['BirdCall']:
+        calls = BirdCall.objects.filter(bird_species_id=species_id)
+        return list(calls)
+    
+    @staticmethod
+    def get_photos_with_wiki() -> List['BirdPhoto']:
+        photos = BirdPhoto.objects.select_related('wiki').filter(wiki__isnull=False)
+        return list(photos)
+    
+    @staticmethod
+    def filter_photos_by_photographer(photographer_name: str) -> List['BirdPhoto']:
+        photos = BirdPhoto.objects.filter(photographer__icontains=photographer_name).select_related('wiki')
+        return list(photos)
+    
+    @staticmethod
+    def get_wiki_with_media(wiki_id: int) -> 'Wiki':
+        wiki = Wiki.objects.filter(id=wiki_id).prefetch_related(
+            Prefetch('birdphoto_set', queryset=BirdPhoto.objects.only('id', 'name', 'image')),
+            Prefetch('birdcall_set', queryset=BirdCall.objects.only('id', 'name'))
+        ).first()
+        return wiki
+    
+    @staticmethod
+    def get_all_photos_grouped_by_species() -> List['BirdPhoto']:
+        photos = BirdPhoto.objects.select_related('bird_species').filter(bird_species__isnull=False).order_by('bird_species__name')
+        return list(photos)
